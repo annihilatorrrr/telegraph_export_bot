8db2cea5a18eb38e7ac5d12a6f3081bcbdfd18be
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-11-17 10:36:13 -0800

    commit

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
 import os
 import sys
 
-EXE_FILE = 'export'
-
 def kill():
-	os.system("ps aux | grep ython | grep %s | awk '{print $2}' | xargs kill -9" % EXE_FILE)
+	os.system("ps aux | grep ython | grep export.py | awk '{print $2}' | xargs kill -9")
 
 def setup(arg = ''):
+	kill()
 	if arg == 'kill':
-		kill()
 		return
 		
-	RUN_COMMAND = "nohup python3 -u %s.py &" % EXE_FILE
-
-	try:
-		import yaml
-		with open('TELEGRAPH_TOKENS') as f:
-			TELEGRAPH_TOKENS = yaml.load(f, Loader=yaml.FullLoader)
-	except:
-		with open('TELEGRAPH_TOKENS', 'w') as f:
-			f.write(yaml.dump({}, sort_keys=True, indent=2))
-
-	kill()
+	RUN_COMMAND = "nohup python3 -u export.py &" % 
 
 	if arg.startswith('debug'):
 		os.system(RUN_COMMAND[6:-2])
